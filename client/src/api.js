77a7be1c7e4c8379d2f62b51cf87c9ed27700ac5
Elcoosp <elcoosp@gmail.com>
2018-03-30 15:45:04 +0200

    Add a function listening or add stock symbol error event on the socket

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,10 @@
 import openSocket from 'socket.io-client'
 const socket = openSocket('/')
 
-export const subscribeToCharts = cb =>
-  socket.on('chartsData', data => cb(null, data))
+export const subscribeToCharts = cb => {
+  socket.on('chartsData', data => console.log(data))
+}
 
 export const addStockSymbol = symbol => socket.emit('addStockSymbol', symbol)
+export const subscribeToAddStockSymbolError = cb =>
+  socket.on('addStockSymbolError', error => cb(error))
