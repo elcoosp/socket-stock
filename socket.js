2f17a1536d739a74b6303ca7e856368db082dd16
Elcoosp <elcoosp@gmail.com>
2018-03-30 23:28:10 +0200

    Refactor getChartsData so that the fetched data from the api is now consumable by client
    Read comment to understand this Behemoth
    
    Need to split this file

--- socket.js
+++ socket.js
@@ -1,31 +1,62 @@
 const request = require('request-promise'),
   socketIO = require('socket.io'),
   to = require('await-to-js').to,
+  _ = require('lodash'),
   mockData = require('./mockData'),
-  { log } = require('./utils'),
+  { log, getFirstKey } = require('./utils'),
   StockSymbol = require('./models/StockSymbol')
 
 const getChartsData = async () => {
   ;[dbError, stockSymbols] = await to(StockSymbol.find())
 
-  // map all symbols to an array of url request
-  // wait for all to resolve or reject then send back the error/data
   if (dbError) return { error: 'Can not get charts data' }
   ;[e, chartsData] = await to(
-    Promise.all([...stockSymbols.map(({ url }) => request(url))]).then(res =>
-      res.map(arr => JSON.parse(arr))
-    )
+    (() => {
+      // Make a string separated by comma of the names from the retrieved stockSymbols in DB
+      // Then create the url and request the batched payload
+      const stockSymbolsNames = stockSymbols.map(({ name }) => name)
+      const URL = `https://api.iextrading.com/1.0/stock/market/batch?symbols=${stockSymbolsNames.join()}&types=chart&range=1m`
+      return request(URL)
+        .then(res => JSON.parse(res))
+        .then(allSymbols => {
+          //Then we create an array of all dates
+          const dates = getFirstKey(getFirstKey(allSymbols)).map(
+            ({ date }) => date
+          )
+          //We iterate over the dates
+          const output = dates.reduce((acc, date, dateIndex) => {
+            // Creating a date point object with the date as prop
+            const datePoint = {
+              date
+            }
+            //For each symbol we add a key to the datepoint equal to the corresponding symbol data
+            // The trick is that we access
+            stockSymbolsNames.forEach(
+              (s, i) => (datePoint[s] = allSymbols[s].chart[dateIndex])
+            )
+            //FInally we push the whole object in the accumulator
+            acc.push(datePoint)
+            return acc
+          }, [])
+
+          return output
+        })
+    })()
   )
 
-  return e ? { error: 'Can not get charts data' } : chartsData
+  return new Promise(
+    (resolve, reject) =>
+      e ? reject('Can not get charts data') : resolve(chartsData)
+  )
 }
-
 const emitChartsData = async socketOrIo => {
   // Await to get all the charts data, then broadcast it
   ;[e, chartsData] = await to(getChartsData())
 
   return e
-    ? { error: 'An error occured while retrieving charts data' }
+    ? socketOrIo.emit('chartsDataError', {
+        error: e
+      })
     : socketOrIo.emit('chartsData', chartsData)
 }
 
@@ -34,8 +65,9 @@ const registerAddStockSymbol = (socket, io) =>
     const emitError = msg => socket.emit('addStockSymbolError', { error: msg })
     //Check if the symbol is not already registered in DB
     ;[dbError, alreadyInDB] = await to(StockSymbol.findOne({ name }))
-    if (dbError || alreadyInDB)
-      return emitError('Symbol is already here or an error occured')
+    if (dbError) return emitError('An error occured')
+    if (alreadyInDB)
+      return emitError('Symbol is already in the chart')
 
       // Check if symbol exist in the API
     ;[symbolNotFound, data] = await to(
@@ -55,7 +87,7 @@ const registerAddStockSymbol = (socket, io) =>
 const launchSocket = server => {
   io = socketIO(server)
   io.on('connection', async socket => {
-    log('green', 'Client cozzznnected to socket')
+    log('green', 'Client connected to socket')
     socket.on('error', e => log('red', `SOCKET ERROR : ${e}`))
 
     emitChartsData(socket)
