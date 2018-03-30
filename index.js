9496223ae1ff0d937ed6432570e3da3137e3d6e7
Elcoosp <elcoosp@gmail.com>
2018-03-30 15:46:14 +0200

    Move apart the socket logic from the index file

--- index.js
+++ index.js
@@ -3,15 +3,13 @@
 // Global modules require and dotenv initialization
 const express = require('express'),
   dotenv = require('dotenv').config(),
-  socketIO = require('socket.io'),
   path = require('path'),
   mongoose = require('mongoose'),
   helmet = require('helmet'),
   mongoSanitize = require('express-mongo-sanitize')
 
 // App modules and variables
-const { emitChartsData, addStockSymbol } = require('./handlers'),
-  mockData = require('./mockData'),
+const launchSocket = require('./socket'),
   { log } = require('./utils'),
   { PORT = 5000, MONGODB_URI } = process.env,
   INDEX = path.join(__dirname, 'client/build/index.html'),
@@ -26,24 +24,12 @@ mongoose
 
 // App setup
 const app = express(),
-  server = require('http').createServer(app),
-  io = socketIO(server)
+  server = require('http').createServer(app)
 
 app
   .use(express.static(PUBLIC_FOLDER))
   .get('*', (req, res) => res.sendFile(INDEX))
 
-// Web socket
-io.on('connection', socket => {
-  log('green', 'Client connected to socket')
-  socket.on('error', e => log('red', `SOCKET ERROR : ${e}`))
-  socket.on('addStockSymbol', name => addStockSymbol(socket, name))
-
-  socket.emit('chartsData', emitChartsData())
-
-  socket.on('disconnect', () =>
-    log('yellow', 'Client disconnected from socket')
-  )
-})
+launchSocket(server)
 
 server.listen(PORT, () => console.log(`Listening on ${PORT}`))
