267737a418b31e1b5383661df115be6fa01f68a6
Elcoosp <elcoosp@gmail.com>
2018-03-31 11:22:27 +0200

    Socket error subscribers

--- client/src/api.js
+++ client/src/api.js
@@ -1,10 +1,16 @@
 import openSocket from 'socket.io-client'
 const socket = openSocket('/')
 
-export const subscribeToCharts = cb => {
-  socket.on('chartsData', cb)
-}
+export const subscribeToCharts = cb => socket.on('chartsData', cb)
 
-export const addStockSymbol = symbol => socket.emit('addStockSymbol', symbol)
+export const addStockSymbol = name => socket.emit('addStockSymbol', name)
+export const subscribeToChartsDataError = cb =>
+  socket.on('chartsDataError', error => cb(error))
+
 export const subscribeToAddStockSymbolError = cb =>
   socket.on('addStockSymbolError', error => cb(error))
+
+export const subscribeToRemoveStockSymbolError = cb =>
+  socket.on('removeStockSymbolError', error => cb(error))
+
+export const removeStockSymbol = name => socket.emit('removeStockSymbol', name)
